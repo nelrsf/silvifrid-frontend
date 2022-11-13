f91087e1d8740062d290f6afcc782be8113284fa
nel.rsf <nsantamaria@tiresia.co>
2022-11-13 14:41:21 -0500

    apuntando api rest hacia servidor en vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
  */
 export const environment = {
   production: true,
-  api_courier_url: "https://sf-courier.onrender.com",
+  api_courier_url: "https://silvifrid-courier-3nmea3i3o-nelrsf.vercel.app",
   api_catalog_url: "https://sf-catalog.onrender.com",
   avatars_main_route: "./../../../../../assets/img/avatars/",
   max_avatar_number_images: 13
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@
  */
 export const environment = {
   production: false,
-  api_courier_url: "https://sf-courier.onrender.com",
+  api_courier_url: "https://silvifrid-courier-3nmea3i3o-nelrsf.vercel.app",
   api_catalog_url: "https://sf-catalog.onrender.com",
   avatars_main_route: "./../../../../../assets/img/avatars/",
   max_avatar_number_images: 13
