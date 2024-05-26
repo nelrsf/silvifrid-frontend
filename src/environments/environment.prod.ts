4dc0caa0cb43d61a9c2c2267803f4b1290a8c612
nelrsf <nel.rsf@gmail.com>
2024-05-26 10:33:21 -0500

    add payment url 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,6 @@ export const environment = {
   production: true,
   api_url: "https://silvifrid-gateway.vercel.app",
   avatars_main_route: "./../../../../../assets/img/avatars/",
-  max_avatar_number_images: 13
+  max_avatar_number_images: 13,
+  payment_redirect_url: "https://silvifrid-frontend.vercel.app"
 };
