2d78508c71dcb0f4505d366ca844b385bc51e40b
nelrsf <nel.rsf@gmail.com>
2024-07-23 22:52:46 -0500

    switch to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,5 +7,5 @@ export const environment = {
   avatars_main_route: "./../../../../../assets/img/avatars/",
   max_avatar_number_images: 13,
   payment_redirect_url: "https://silvifrid-frontend.vercel.app",
-  bold_api_key: "8DMcjDWPd7AjcrIfrUAlvSUL1VY_R1iWLhaG3mHiu4k"
+  bold_api_key: "UyIlJjB39tvi6sizGUHv2VUM8vWieUZMmILRuSwNUzA"
 };
